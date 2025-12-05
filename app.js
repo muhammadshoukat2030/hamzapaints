2a142edf5f455a752e52ceff645499a3ec87d005
Musadiqaman <musadiqaman2006@gmail.com>
2025-12-05 05:20:19 -0800

    fuck suck

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use(
 // =======================================================
 // Allowed origins
 const allowedOrigins = process.env.NODE_ENV === "production"
-  ? ["https://paintsstore.vercel.app/"]   // Add your production domain(s) here
+  ? ["https://paintsstore.vercel.app"]   // Add your production domain(s) here
   : ["http://localhost:3000"];           // Localhost for dev
 
 // ===== CORS Middleware =====
